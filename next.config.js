951d4909477e7a05bcf35613c4714c88f4f10274
unknown <adham111mahfoud@gmail.com>
2024-03-22 11:28:16 +0300

    123

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const createNextIntlPlugin = require('next-intl/plugin');
- 
 const withNextIntl = createNextIntlPlugin();
- 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-};
- 
-module.exports = withNextIntl(nextConfig);
+
+module.exports = withNextIntl({
+  trailingSlash: true
+});
